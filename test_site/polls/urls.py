185032a182ee7ff90b4abbbb3373a1682e53784e
YO-G <yonath-g@hotmail.fr>
2020-05-10 18:13:06 +0200

    New views generique more simple

--- test_site/polls/urls.py
+++ test_site/polls/urls.py
@@ -4,15 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # http://127.0.0.1:8000/polls/
-    path('', views.index, name='index'),
-    # ex: http://127.0.0.1:8000/polls/1
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: http://127.0.0.1:8000/polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: http://127.0.0.1:8000/polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    #
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
-
